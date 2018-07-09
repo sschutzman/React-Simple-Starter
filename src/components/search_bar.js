3516e72bf32da5c4889c83874fbe2bbcc9053000
sschutzman <$t735815>
2018-07-09 11:06:16 -0500

    manipulated state and cleaned up ES6

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
-    render(){
-        return <input />;
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
     }
+    render(){
+        return (
+            <div>
+                <input onChange={(event) => this.setState({term: event.target.value})} />
+                Value of the input: {this.state.term}
+            </div>
+        );
+    }
+
 }
 
 export default SearchBar;
